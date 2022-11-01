c0d57edf7cfb80f90d96efb12336c854ecb2a42e
Radu-Ioan Mihai <rimihai2001@gmail.com>
2022-11-01 17:16:58 +0200

    Player Movement Right+Left
    - Player created
    - Ground created
    - Right and Left movement keys working

--- GameDev - 3D Runner/Assets/PlayerMovement.cs
+++ GameDev - 3D Runner/Assets/PlayerMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+
+    public float forwardForce = 1000f;
+    public float leftRightForce = 500f;
+
     // Start is called before the first frame update
-    void Start()
+    /*void Start()
     {
-        Debug.Log("Hello!");
-    }
+       //Debug.Log("Hello!");
+       //rb.AddForce(0, 200, 500);
+    }*/
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
         
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(leftRightForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-leftRightForce * Time.deltaTime, 0, 0);
+        }
     }
 }
