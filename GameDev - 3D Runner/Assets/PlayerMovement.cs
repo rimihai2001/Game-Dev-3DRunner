9cdef86ac15d6f4a13565d13fbd92bdec4f873b5
Radu-Ioan Mihai <rimihai2001@gmail.com>
2022-11-12 02:35:31 +0200

    Jump done

--- GameDev - 3D Runner/Assets/PlayerMovement.cs
+++ GameDev - 3D Runner/Assets/PlayerMovement.cs
@@ -10,7 +10,10 @@ public class PlayerMovement : MonoBehaviour
     //Varibles declared public so they can be changed from the Unity frontend instead of changing directly into the code
     public float forwardForce = 1000f;
     public float sidewayForce = 500f;
+    public float jumpForce = 100f;
+    private bool playerOnGround = true;
     
+
 
     //Update function that is called once per frame
     void FixedUpdate()
@@ -29,5 +32,30 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0);
         }
+
+        //Jump activated by "W" key if the player is on the ground
+        if (Input.GetKey("w") && playerOnGround)
+        {
+            rb.AddForce(0, jumpForce, 0, ForceMode.Impulse);
+        }
     }
+
+    //Function that detects if the player is on the ground and changes the variable value to true
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            playerOnGround = true;
+        }
+    }
+
+    //Function that detects if the player is not on the ground anymore and changes the variable accordingly
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            playerOnGround = false;
+        }
+    }
+
 }
