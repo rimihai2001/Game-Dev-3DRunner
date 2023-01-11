45f3ea333f51754504faa5e1d597a2e151a43018
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-01-11 21:31:31 +0200

    Death Sound added

--- GameDev - 3D Runner/Assets/Scripts/PlayerCollision.cs
+++ GameDev - 3D Runner/Assets/Scripts/PlayerCollision.cs
@@ -7,6 +7,8 @@ public class PlayerCollision : MonoBehaviour
     //Variable to link the script to the position of the player
     public Transform player;
 
+    public AudioSource deathSound;
+
     void FixedUpdate()
     {
         if (player.position.y < -10)
@@ -25,10 +27,10 @@ public class PlayerCollision : MonoBehaviour
         //If statement that disables the movement and stops the game if the player collides into a "GameOverObstacle" tag object
         if (collisionInfo.gameObject.tag == "GameOverObstacle")
         {
+            deathSound.Play();
             pm.enabled = false;
             // change value of gameOver bool to True in order for the GameOverPanel to be visible
             PlayerManager.gameOver = true;
-            // Debug.Log("GAME OVER!");
         }
     }
 }
